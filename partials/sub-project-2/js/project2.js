0ad2da7fd9f5a552482b113c5d7dd2a941d9366c
hiranwj <hiranwj.connect@gmail.com>
2022-10-14 10:25:03 +0530

    Add sub-project-4

--- partials/sub-project-2/js/project2.js
+++ partials/sub-project-2/js/project2.js
@@ -1,9 +1,10 @@
 const boxElm = document.querySelector('#box');
 
 let x = 0;
+// let y = 0;
 const startY = (innerHeight-boxElm.offsetHeight) / 2;
 
 srtInterval(() => {
-    const y = startY + Math.sin(x++ / 180) * 50;
+    const y = startY + Math.sin(x++/180)*50;
     boxElm.style.top = `${y}px`;
 }, 5);
--- partials/sub-project-3/js/project3.js
+++ partials/sub-project-3/js/project3.js
@@ -1,1 +1,7 @@
-console.log('Web API Experiment - Sub Project 2');
+let round1 = document.getElementById('circle1')
+
+round1.style.animation = 'circle1 6s linear infinite'
+
+let round2 = document.getElementById('circle2')
+
+round2.style.animation = 'circle2 6s linear infinite'
